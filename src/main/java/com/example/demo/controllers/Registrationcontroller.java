c4750f3d9e064fe1eaec1aa42912d41cca162136
prasadveeramallu <durga93@student.fdu.edu>
2018-09-12 16:15:49 -0400

    secondcommit

--- src/main/java/com/example/demo/controllers/Registrationcontroller.java
+++ src/main/java/com/example/demo/controllers/Registrationcontroller.java
@@ -4,10 +4,7 @@ import com.example.demo.models.Registration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
@@ -27,8 +24,8 @@ public ModelAndView registration()
 public ModelAndView registrationDetails(@ModelAttribute Registration registration)
 {
     ModelAndView modelAndView=new ModelAndView();
-
-   ResponseEntity<Registration[]> registrationResponseEntity=restTemplate.postForEntity("url",registration,Registration[].class);
+    System.out.println("register");
+   ResponseEntity<Registration[]> registrationResponseEntity=restTemplate.postForEntity("http://localhost:8080/registerdetails",registration,Registration[].class);
 
    int statuscode=registrationResponseEntity.getStatusCodeValue();
    if(statuscode>=200 && statuscode<=299)
